5226a7e3686a42d1581ee75fbf97a3a1562dae95
pacmandoh <pacmandoh@outlook.com>
2024-01-01 20:38:37 +0800

    feat: index page and nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  extends: ['@nuxt/ui-pro'],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/google-fonts',
+    'nuxt-og-image',
+    '@vueuse/nuxt'
+  ],
+  ui: {
+    icons: ['heroicons', 'simple-icons', 'ph', 'mdi']
+  },
+
+  // font-family
+  fontMetrics: {
+    fonts: ['DM Sans']
+  },
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    families: {
+      'DM+Sans': [400, 500, 600, 700]
+    }
+  },
+  devtools: { enabled: true },
+  typescript: { strict: false }
 })
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
+
+defineOgImage({
+  component: 'Landing',
+  //@ts-ignore
+  title: page.value.title,
+  description: page.value.description
+})
+</script>
+
+<template>
+  <div>
+    <h1>
+      Welcome! {{ page.title }}
+    </h1>
+    <p>{{ page.description }}</p>
+  </div>
+</template>
